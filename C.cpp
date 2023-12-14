22512a7b59d0aaad3f02129337c0773de6c77b5d
nquinbla <nquinbla@myuax.com>
2023-12-14 12:52:09 +0100

    a

--- C.cpp
+++ C.cpp
@@ -39,4 +39,52 @@ void agregarMateria(struct Estudiante* estudiante, const char* materia) {
 }
 
 // Funcion para eliminar materias de un estudiante
+void eliminarMateria(struct Estudiante* estudiante, const char* materia) {
+    for (int i = 0; i < estudiante->numMaterias; i++) {
+        if (strcmp(estudiante->materias[i], materia) == 0) {
+        }
+            for (int j = i; j < estudiante->numMaterias - 1; j++)
+            {
+                strcpy(estudiante->materias[j], estudiante->materias[j + 1]);
+            }
+            estudiante->numMaterias--;
+            break;
+        }
+    }
+}
 
+// Estructura asistencia
+struct Asistencia {
+    char fecha[30];
+    int materia;
+    int estado;
+};
+
+// Funcion para registrar asisencia de un estudiante
+void registrarAsistencia(struct Estudiante* estudiante, struct Asistencia* asistencia) {
+    for (int i = 0; i < estudiante->numMaterias; ++i) {
+        if (strcmp(estudiante->materias[i], asistencia->materia) == 0) {
+            // Aquí puedes implementar lógica adicional para manejar la asistencia
+            printf("Asistencia registrada para la materia %s\n", asistencia->materia);
+            return;
+        }
+    }
+    fprintf(stderr, "El estudiante no está inscrito en la materia de asistencia.\n");
+}
+
+// Funcion principal
+int main() {
+    struct Estudiante estudiante = {"Nerea", 18, 9.5, {"Matematicas", "Programacion"};"};
+    mostrarEstudiante(&estudiante);
+
+    agregarMateria(&estudiante, "Fisica");
+    mostrarEstudiante(&estudiante);
+
+    eliminarMateria(&estudiante, "Matematicas");
+    mostrarEstudiante(&estudiante);
+
+    struct Asistencia asistencia = {"2021-09-01", "Programacion", 1};
+    registrarAsistencia(&estudiante, &asistencia);
+
+    return 0;
+}
